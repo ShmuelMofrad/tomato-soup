e752dbb0276842b8e10ed6b61e8c447721980bdd
samuel.mofrad <samuel@unus.one>
2022-04-03 18:51:01 +0200

    step 4

--- firstCode/src/app.js
+++ firstCode/src/app.js
@@ -2,3 +2,6 @@
 const userInputElement = document.querySelector('input');
 const todoListElement = document.querySelector('ul');
 const addButtonElement = document.querySelector('button');
+
+// Add event listener to button
+addButtonElement.addEventListener('click', addNewTodo);
