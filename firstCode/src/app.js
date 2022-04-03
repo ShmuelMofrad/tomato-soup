ee622034a18331c3057e8afd9b2381bb961c4b66
samuel.mofrad <samuel@unus.one>
2022-04-03 18:48:01 +0200

    step 3

---
+++ firstCode/src/app.js
@@ -0,0 +1,4 @@
+// Read html elemnts
+const userInputElement = document.querySelector('input');
+const todoListElement = document.querySelector('ul');
+const addButtonElement = document.querySelector('button');
